6cab6ac4ac54e1909a5f0f955f96ba640ad7d0f6
SeungHwan-Lee <sh981013s@gmail.com>
2021-12-01 16:09:53 +0900

    feat: music-player script

---
+++ Music-Player/script.js
@@ -0,0 +1,105 @@
+const musicContainer = document.getElementById('music-container');
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+
+const audio = document.getElementById('audio');
+const progress = document.getElementById('progress');
+const progressContainer = document.getElementById('progress-container');
+const title = document.getElementById('title');
+const cover = document.getElementById('cover');
+
+
+// Song titles
+const songs = ['hey', 'summer', 'ukulele'];
+
+// Keep track of song
+let songIndex = 2;
+
+// Update song details
+const loadSong = (song) => {
+    title.innerText = song;
+    audio.src = `music/${song}.mp3`
+    cover.src = `images/${song}.jpg`
+}
+
+// Play song
+const playSong = () => {
+    musicContainer.classList.add('play');
+    playBtn.querySelector('i.fas').classList.remove('fa-play');
+    playBtn.querySelector('i.fas').classList.add('fa-pause');
+
+    audio.play();
+}
+
+// Pause song
+const pauseSong = () => {
+    musicContainer.classList.remove('play');
+    playBtn.querySelector('i.fas').classList.remove('fa-pause');
+    playBtn.querySelector('i.fas').classList.add('fa-play');
+
+    audio.pause();
+}
+
+// Previous song
+const prevSong = () => {
+    songIndex--;
+    if(songIndex < 0) {
+        songIndex = songIndex.length-1;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+// Next song
+const nextSong = () => {
+    songIndex++;
+    if(songIndex > songs.length-1) {
+        songIndex = 0;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+// Update progress bar
+const updateProgress = (e) => {
+    const { duration, currentTime } = e.srcElement;
+    const percentage = Math.floor(currentTime/duration * 100);
+    progress.style.width = `${percentage}%`;
+}
+
+// Initially load song details into DOM
+loadSong(songs[songIndex]);
+
+// Set progress bar
+const setProgress = (e) => {
+    const width = progressContainer.clientWidth;
+    const clickX = e.offsetX;
+    const duration = audio.duration;
+
+    audio.currentTime = (clickX / width) * duration;
+}
+
+// Event listeners
+playBtn.addEventListener('click', () => {
+    const isPlaying = musicContainer.classList.contains('play');
+
+    if(isPlaying) {
+        pauseSong();
+    } else {
+        playSong();
+    }
+})
+
+// Change song
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+
+// Time/song update
+audio.addEventListener('timeupdate', updateProgress);
+
+// Click on progress bar
+progressContainer.addEventListener('click', setProgress);
+
+// Song ends
+audio.addEventListener('ended', nextSong);
