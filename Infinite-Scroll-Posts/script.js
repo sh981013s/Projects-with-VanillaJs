5c1df22fcdbe7d30ed7fbab9be76a7d1fa47b777
SeungHwan-Lee <sh981013s@gmail.com>
2021-12-01 18:14:36 +0900

    feat: infinite-scroll main

--- Infinite-Scroll-Posts/script.js
+++ Infinite-Scroll-Posts/script.js
@@ -2,7 +2,7 @@ const postsContainer = document.getElementById('post-container');
 const loading = document.querySelector('.loader');
 const filter = document.getElementById('filter');
 
-let limit = 3;
+let limit = 6;
 let page = 1;
 
 
